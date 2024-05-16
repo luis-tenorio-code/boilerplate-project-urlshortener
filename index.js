3965929d23c7f9b97035b6ebf2bf6fb0d2a82138
Luis Tenorio <luisprograming0@gmail.com>
2024-05-16 04:41:18 +0000

    feat: Add URL shortening functionality to the API

--- index.js
+++ index.js
@@ -10,32 +10,51 @@ app.use(cors());
 
 // Middleware para analizar solicitudes POST JSON
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Objeto para almacenar las URL originales y sus URL cortas correspondientes
+const urlDatabase = {};
+
+// middleware para validar la URL
+const validateUrl = (req, res, next) => {
+  const { url } = req.body;
+  const urlRegex = /^(ftp|http|https):\/\/[^ "]+$/;
+  if (urlRegex.test(url)) {
+    next();
+  } else {
+    res.json({ error: 'invalid URL' });
+  }
+};
+
+// Ruta para recibir URL larga y generar URL corta
+app.post('/api/shorturl', validateUrl, (req, res) => {
+  // Puedes POST una URL a /api/shorturl y obtener una respuesta JSON con propiedades original_url y short_url. Aquí hay un ejemplo: { original_url : 'https://freeCodeCamp.org', short_url : 1}.
+  const { url } = req.body;
+  const shortUrl = crypto.randomUUID();
+  urlDatabase[shortUrl] = url;
+  res.json({ original_url: url, short_url: shortUrl });
+});
+
+// Ruta para redirigir la URL corta a la URLoriginal
+app.get('/api/shorturl/:shortUrl', (req, res) => {
+  const { shortUrl } = req.params;
+  const url = urlDatabase[shortUrl];
+  if (url) {
+    res.redirect(url);
+  } else res.json({ error: 'No short URL found for the given input' });
+});
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Ruta para recibir URL larga y generar URL corta
-app.post('/api/shorturl', (req, res) => {
-    const originalUrl = req.body.url;
-    // Aquí implementa la lógica para acortar la URL
-    // Devuelve la URL original y la URL corta en formato JSON
-    res.json({ original_url: originalUrl, short_url: '1' });
-});
-// Ruta para redirigir la URL corta a la URL original
-app.get('/api/shorturl/:shortUrl', (req, res) => {
-    const shortUrl = req.params.shortUrl;
-    // Aquí implementa la lógica para redirigir a la URL original
-    res.redirect('https://www.example.com');
-});
-
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
+app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
